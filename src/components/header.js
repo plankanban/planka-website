bccf3fc55c32b3d182a6dc1c1a7dfce5d16efd54
Maksim Eltyshev <meltyshev@gmail.com>
2023-10-26 01:08:29 +0200

    fix: Update install link

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Header = () => (
   <header>
-    <a href="https://github.com/plankanban/planka#deploy" target="_blank" rel="noopener noreferrer">Install</a>
+    <a href="https://docs.planka.cloud/docs/intro/" target="_blank" rel="noopener noreferrer">Install</a>
     <a href="https://plankanban.github.io/planka" target="_blank" rel="noopener noreferrer">Demo</a>
     <a href="https://github.com/plankanban/planka/blob/master/LICENSE" target="_blank" rel="noopener noreferrer">License</a>
   </header>
