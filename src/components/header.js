dc3942b4762916832bb8a0ffc5f56c09dbe34624
Maksim Eltyshev <meltyshev@gmail.com>
2024-03-22 01:30:06 +0100

    feat: Add link to GitHub repository

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,7 @@ const Header = () => (
     <a href="https://docs.planka.cloud/docs/intro/" target="_blank" rel="noopener noreferrer">Install</a>
     <a href="https://plankanban.github.io/planka" target="_blank" rel="noopener noreferrer">Demo</a>
     <a href="https://github.com/plankanban/planka/blob/master/LICENSE" target="_blank" rel="noopener noreferrer">License</a>
+    <a href="https://github.com/plankanban/planka" target="_blank" rel="noopener noreferrer">GitHub</a>
   </header>
 )
 
